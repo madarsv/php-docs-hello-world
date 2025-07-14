680e8954c7ab934b6b7b503ddc993963bdceba83
madarsv <63092244+madarsv@users.noreply.github.com>
2025-07-14 09:46:50 +0300

    Test Db read data

--- testdb.php
+++ testdb.php
@@ -1,3 +1,30 @@
 <?php
-echo "Db<br>\n";
+$host = getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USERNAME');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DBNAME');
 
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Query
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+
+// Fetch data
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close connection
+mysqli_close($conn);
+?>
